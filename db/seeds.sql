2a25d087791b0b3e157b2aa76f701eaa7f293220
Bhaskar Tripathi <bhaskar.tripathi@outlook.com>
2020-02-20 18:27:37 -0800

    draft

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO burger VALUES (null, "Thrill of the Grill", false),
+							(null, "Fervor Burger", false),
+                            (null, "Bayside Burgers", false),
+                            (null, "Burger Buzz", false),
+                            (null, "IceBurger", false),
+                            (null, "Queen Bee Burgers", false),
+                            (null, "Grill for Gold", false),
+                            (null, "Build Up Burger", false),
+                            (null, "Bunrise Burgers", false);
